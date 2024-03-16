d2381b45f4f77893a3065e14ca19c31aef13d14d
DeNaice <dennis.krs@hotmail.de>
2024-03-16 22:31:10 +0100

    Walking done
    TODO is now Fighting

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,28 +1,75 @@
-extends CharacterBody2D
+extends Node2D
 
+@onready var tile_map = $"../TileMap"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var astar_grid: AStarGrid2D
+var current_id_path: Array[Vector2i]
+var current_point_path: PackedVector2Array
+var target_position: Vector2
+var is_moving: bool
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = Vector2(16, 16)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.update()
+
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x,
+				y + tile_map.get_used_rect().position.y
+			)
+			
+			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+
+func _input(event):
+	if event.is_action_pressed("move") == false:
+		return
+	
+	var id_path
+	
+	if is_moving:
+		id_path = astar_grid.get_id_path(
+			tile_map.local_to_map(target_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		)
+	else:
+		id_path = astar_grid.get_id_path(
+			tile_map.local_to_map(global_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		).slice(1)
+	
+	if id_path.is_empty() == false:
+		current_id_path = id_path
+		
+		current_point_path = astar_grid.get_point_path(
+			tile_map.local_to_map(target_position),
+			tile_map.local_to_map(get_global_mouse_position())
+		)
+		
+		for i in current_point_path.size():
+			current_point_path[i] = current_point_path[i] + Vector2(8, 8)
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if current_id_path.is_empty():
+		return
+		
+	if is_moving == false:
+		target_position = tile_map.map_to_local(current_id_path.front())
+		is_moving = true
+	
+	global_position = global_position.move_toward(target_position, 1)
+	
+	if global_position == target_position:
+		current_id_path.pop_front()
+		
+		if current_id_path.is_empty() == false:
+			target_position = tile_map.map_to_local(current_id_path.front())
+		else:
+			is_moving = false
